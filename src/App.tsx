98a36ceb6f70fb5ea24ee45dea094fef82279ea7
Basilio <basilioherimiharisoa@gmail.com>
2026-07-01 08:17:51 +0300

    Ajout d'une animation fluide

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Devis from "./pages/Devis";
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <div className="bg-white text-dark font-sans">
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/devis" element={<Devis />} />
-        </Routes>
-      </main>
+      <AnimatePresence mode="wait">
+        <motion.main
+          key={location.pathname}
+          initial={{ opacity: 0, y: 12 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -12 }}
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+        >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/devis" element={<Devis />} />
+          </Routes>
+        </motion.main>
+      </AnimatePresence>
     </div>
   );
 }
